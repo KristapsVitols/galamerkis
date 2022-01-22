8405f847a5c9720590bd86e0e1dc625e5619614c
kristapsvitols <kristapsvitols91@gmail.com>
2022-01-22 13:05:58 +0200

    par mums layout

--- client/pages/par-mums.vue
+++ client/pages/par-mums.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-        hello there. again
+        <Header/>
+        <p>hello there.</p>
+        <Footer/>
     </div>
 </template>
+<script>
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+export default {
+    components: {Footer, Header}
+}
+</script>
