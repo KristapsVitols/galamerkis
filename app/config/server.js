227fbd2b2598387c2d8a2b005679be01d581983b
kristapsvitols <kristapsvitols91@gmail.com>
2022-01-26 11:20:26 +0200

    simplify dev environment due to some rekt recent changes

--- app/config/server.js
+++ app/config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('STRAPI_BASE_URL', 'https://galamerkis.local/content'),
+  url: env('STRAPI_BASE_URL', 'http://127.0.0.1/content'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '268602d55563dcf0daeb1ac3c314d269'),
     },
-    url: env('STRAPI_DASHBOARD_URL', 'https://galamerkis.local/dashboard'),
+    url: env('STRAPI_DASHBOARD_URL', 'http://127.0.0.1/dashboard'),
   },
 });
--- dev.js
+++ dev.js
@@ -1,19 +1,12 @@
 const util = require('util');
-const fs = require('fs');
 const {exec} = require('child_process');
 
-const readFile = util.promisify(fs.readFile);
-const appendFile = util.promisify(fs.appendFile);
-const access = util.promisify(fs.access);
-const mkdir = util.promisify(fs.mkdir);
 const run = util.promisify(exec);
 
 class DeveloperEnvironment {
     async setup() {
         await this.installDependencies();
         await this.removeDocker();
-        await this.addHosts();
-        await this.addCertificates();
         await this.buildDocker();
         await this.startDocker();
     }
@@ -55,12 +48,6 @@ class DeveloperEnvironment {
     }
 
     async buildDocker() {
-        try {
-            await run('docker network create galamerkis-proxy');
-        } catch (err) {
-            //
-        }
-
         console.log('Building containers... This will take a while...');
         const res = await run('docker-compose -f docker-compose.dev.yml build --no-cache');
         console.log(res.stdout || res.stderr);
@@ -73,35 +60,6 @@ class DeveloperEnvironment {
         const res = await run('docker-compose -f docker-compose.dev.yml up -d');
         console.log(res.stdout || res.stderr);
         console.log('Everything is up and running! Hopefully...');
-    }
-
-    async addCertificates() {
-        console.log('Setting up local certificates...');
-        try {
-            await access('./certs', fs.constants.F_OK);
-            console.log('Already have certificates, skipping!');
-        } catch (err) {
-            await mkdir('./certs');
-            await run('cd ./certs');
-            await run('npm run ssl');
-            await run('cd ./certs && mv ssl.crt galamerkis.local.crt && mv ssl.key galamerkis.local.key');
-            console.log('Certificates added!');
-        }
-    }
-
-    async addHosts() {
-        await run('sudo chmod 777 /etc/hosts');
-        console.log('Setting up hosts file...');
-        const test = await readFile('/etc/hosts');
-
-        // already set up.
-        if (test.toString().includes('galamerkis')) {
-            return console.log('Hosts file already set up. Skipping...');
-        }
-
-        console.log('Writing hosts file...');
-        await appendFile('/etc/hosts', '\n# Galamerkis\n127.0.0.1 galamerkis.local\n127.0.0.1 www.galamerkis.local');
-        console.log('Hosts file setup!');
     }
 
     async installDependencies() {
