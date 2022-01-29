54f54b30883f05a53c4881c6586e6f709c296100
kristapsvitols <kristapsvitols91@gmail.com>
2022-01-29 12:15:33 +0200

    author image

--- client/components/OneThirdPost.vue
+++ client/components/OneThirdPost.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
             <div class="flex items-center justify-between">
-                <img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author">
+                <img class="w-8 h-8 rounded-full mr-4 avatar" src="/keitija-krastina.jpg" alt="Keitija Krastina">
                 <p class="text-gray-600 text-xs md:text-sm flex items-center">
                     <span>{{ post.readTime }}</span>
                     <img src="readme-brands.svg" alt="Grāmata" width="20" class="ml-2">
