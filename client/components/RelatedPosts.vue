6d1c64b8bd277a5020ef00aa0ab23530d366f9b9
kristapsvitols <kristapsvitols91@gmail.com>
2022-01-30 12:40:21 +0200

    related posts

---
+++ client/components/RelatedPosts.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="bg-gray-200" id="app">
+        <div class="container w-full max-w-6xl mx-auto px-2 pt-12">
+            <div class="flex flex-wrap justify-center -mx-2">
+                <div v-for="relatedPost in relatedPosts" class="w-full md:w-1/3 px-2 pb-12">
+                    <router-link :to="{name: 'category-slug', params: {category: relatedPost.category.name, slug: relatedPost.slug}}" class="block bg-white rounded shadow-md hover:shadow-lg relative smooth">
+                            <span class="no-underline hover:no-underline">
+                                <img :src="getImageUrl(relatedPost, 'small')" class="h-48 w-full rounded-t shadow"/>
+                                <div class="p-6 h-auto ">
+                                    <p class="text-gray-600 text-xs md:text-sm">{{ relatedPost.category.displayName }}</p>
+                                    <div class="font-bold text-xl text-gray-900">{{ relatedPost.title }}</div>
+                                </div>
+                                <div class="flex items-center justify-between inset-x-0 bottom-0 p-6">
+                                    <img class="w-8 h-8 rounded-full mr-4" src="/keitija-krastina.jpg" alt="Keitija Krastina">
+                                    <p class="text-gray-600 text-xs md:text-sm flex items-center">
+                                        <span>{{ getReadTime(relatedPost.content) }}</span>
+                                        <img src="/readme-brands.svg" alt="Grāmata" width="20" class="ml-2">
+                                    </p>
+                                </div>
+                            </span>
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'RelatedPosts',
+    props: {
+        relatedPosts: {
+            type: Array,
+            required: true,
+        },
+        getReadTime: {
+            type: Function,
+        },
+        getImageUrl: {
+            type: Function,
+        },
+    }
+}
+</script>
--- client/pages/_category/_slug.vue
+++ client/pages/_category/_slug.vue
@@ -2,11 +2,13 @@
     <div>
         <Header height="25vh"/>
         <div class="container w-full max-w-6xl mx-auto -mt-12 text-base font-medium">
-            <router-link :to="{name: 'category', params: {category: post.category.name}}" class="inline-block py-2 px-2 text-gray-800 no-underline hover:text-gray-200">
+            <router-link :to="{name: 'category', params: {category: post.category.name}}"
+                         class="inline-block py-2 px-2 text-gray-800 no-underline hover:text-gray-200">
                 {{ post.category.displayName }}
             </router-link>
-                <span class="text-gray-800">/</span>
-            <router-link :to="{name: 'category-slug', params: {category: post.category.name, slug: post.slug}}" class="inline-block py-2 px-2 text-white no-underline">
+            <span class="text-gray-800">/</span>
+            <router-link :to="{name: 'category-slug', params: {category: post.category.name, slug: post.slug}}"
+                         class="inline-block py-2 px-2 text-white no-underline">
                 {{ post.title }}
             </router-link>
         </div>
@@ -22,7 +24,7 @@
         </div>
 
         <div class="container w-full max-w-6xl mx-auto bg-white bg-cover mt-8 rounded"
-             :style="`background-image:url('/content${post.image.url}'); height: 75vh;`">
+             :style="`background-image:url('${getImageUrl(post, 'large')}'); height: 75vh;`">
         </div>
 
         <div class="container max-w-5xl mx-auto -mt-32">
@@ -34,32 +36,7 @@
             </div>
         </div>
 
-        <div class="bg-gray-200" id="app">
-
-            <div class="container w-full max-w-6xl mx-auto px-2 py-8">
-                <div class=" flex flex-wrap -mx-2">
-                    <div v-for="item in content" class="w-full md:w-1/3 px-2 pb-12">
-                        <div  class=" bg-white rounded shadow-md hover:shadow-lg relative smooth">
-						<span class="no-underline hover:no-underline">
-
-              <template>
-  <img :src=`https://source.unsplash.com/${item.unsplash}` class="h-48 w-full rounded-t shadow"/>
-</template>
-
-								<div class="p-6 h-auto " >
-									<p class="text-gray-600 text-xs md:text-sm">{{ item.category }}</p>
-									<div class="font-bold text-xl text-gray-900">{{item.title}}</div>
-								</div>
-								<div class="flex items-center justify-between inset-x-0 bottom-0 p-6">
-									<img class="w-8 h-8 rounded-full mr-4" src="http://i.pravatar.cc/300" alt="Avatar">
-									<p class="text-gray-600 text-xs md:text-sm">{{ item.min }} MIN READ</p>
-								</div>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <RelatedPosts v-if="relatedPosts.length" :related-posts="relatedPosts" :get-image-url="getImageUrl" :get-read-time="getReadTime"/>
 
         <div class="container px-4 md:px-0 max-w-6xl mx-auto">
             <div class="mx-0 sm:mx-6 mb-16 my-24">
@@ -74,9 +51,11 @@
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import ProgressBar from '../../components/ProgressBar';
+import {Post} from '../../models/post';
+import RelatedPosts from '../../components/RelatedPosts';
 
 export default {
-    components: {ProgressBar, Footer, Header},
+    components: {RelatedPosts, ProgressBar, Footer, Header},
     head() {
         return {
             title: `${this.post.title} | Galamērķis`,
@@ -86,7 +65,11 @@ export default {
                 {hid: 'og:title', property: 'og:title', content: `${this.post.title} | Galamērķis`},
                 {hid: 'og:type', property: 'og:type', content: 'website'},
                 {hid: 'og:site_name', property: 'og:site_name', content: 'Galamērķis'},
-                {hid: 'og:url', property: 'og:url', content: `https://www.galamerkis.lv/${this.post.category.name}/${this.post.slug}`},
+                {
+                    hid: 'og:url',
+                    property: 'og:url',
+                    content: `https://www.galamerkis.lv/${this.post.category.name}/${this.post.slug}`
+                },
                 {hid: 'og:description', property: 'og:description', content: this.post.description},
                 {hid: 'og:image', name: 'og:image', content: `/content${this.post.image?.url}`},
                 {hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image'},
@@ -96,29 +79,7 @@ export default {
         }
     },
     data: () => ({
-        content: [
-            {
-                unsplash: 'KqZ0rBcCYYg',
-                category: 'cosmos',
-                title: 'In city of millions',
-                article: 'A writer is an eerie plastic. Cold drip coffee the storm, street art naked for satan chopper read werribee wildlife richmond tigers, rooftop bars victory vs heart neatly trimmed moustaches warehouse chic aami park, dandenong rooftop cinema melb brunswick and brunswick st richmond tigers, graffiti trams don\'t paint over the banksy\'s.',
-                min: 2.2
-            },
-            {
-                unsplash: 'WxM465oM4j4',
-                category: 'asterisk',
-                title: 'Salamander',
-                article: 'Another world Vangelis network of wormholes laws of physics prime number descended from astronomers. Not a sunrise but a galaxyrise decipherment Drake Equation star stuff harvesting star light cosmic ocean ship of the imagination. Venture dream of the mind\'s eye inconspicuous motes of rock and gas great turbulent clouds cosmic ocean from which we spring? Dream of the mind\'s eye dream of the mind\'s eye gathered by gravity citizens of distant epochs as a patch of light are creatures of the cosmos and billions upon billions upon billions upon billions upon billions upon billions upon billions.',
-                min: 4.42
-            },
-            {
-                unsplash: 'sF8j0Acen78',
-                category: 'raquo',
-                title: 'Cosmos',
-                article: 'Another world Vangelis network of wormholes laws of physics prime number descended from astronomers. Not a sunrise but a galaxyrise decipherment Drake Equation star stuff harvesting star light cosmic ocean ship of the imagination. Venture dream of the mind\'s eye inconspicuous motes of rock and gas great turbulent clouds cosmic ocean from which we spring? Dream of the mind\'s eye dream of the mind\'s eye gathered by gravity citizens of distant epochs as a patch of light are creatures of the cosmos and billions upon billions upon billions upon billions upon billions upon billions upon billions..',
-                min: 4.42
-            },
-        ]
+        relatedPosts: [],
     }),
     async asyncData({$axios, route, error}) {
         const [post] = await $axios.$get(`/content/posts?slug=${route.params.slug}&category.name=${route.params.category}`);
@@ -127,13 +88,32 @@ export default {
             return error({statusCode: 404, message: 'Raksts nav atrasts.'});
         }
 
+        const relatedPosts = await $axios.$get(`/content/posts?category.name=${route.params.category}&id_ne=${post.id}&_limit=3`);
+
         // Super weird bug here. If class is instantiated, it loses it after render and is re-fetching stuff..
-        return {post}
+        return {post, relatedPosts}
     },
+    methods: {
+        getReadTime(content) {
+            // average read time for a human 225 words per minute
+            const wpm = 225;
+            const words = content.trim().split(/\s+/).length;
+            const minutes = Math.ceil(words / wpm);
+
+            return minutes === 1 ? `${minutes} minūte` : `${minutes} minūtes`;
+        },
+        getImageUrl(post, imageSize = '') {
+            if (!imageSize) {
+                return `/content/${post.image.url}`;
+            }
+
+            return `/content/${post.image.formats[imageSize].url}`;
+        },
+    }
 }
 </script>
 <style>
- #content a {
-     color: #11998e;
- }
+#content a {
+    color: #11998e;
+}
 </style>
