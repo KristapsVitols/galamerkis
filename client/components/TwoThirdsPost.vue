4e26dbcf849ecfbb099254f4571f0701a24928b6
kristapsvitols <kristapsvitols91@gmail.com>
2022-03-06 16:15:33 +0200

    perhaps fix

--- client/components/TwoThirdsPost.vue
+++ client/components/TwoThirdsPost.vue
@@ -2,8 +2,8 @@
     <div class="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
         <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
             <router-link :to="{name: 'category-slug', params: {category: post.category.name, slug: post.slug}}" class="flex flex-wrap no-underline hover:no-underline">
-                <img :src="post.getImageUrl('large')" :alt="post.imageAlt" class="h-full w-full rounded-t pb-6" loading="lazy">
-                <p class="w-full text-gray-600 text-xs md:text-sm px-6">{{ post.category.displayName }}</p>
+                <img :src="post.getImageUrl('large')" :alt="post.imageAlt" class="h-full w-full rounded-t" loading="lazy">
+                <p class="w-full text-gray-600 text-xs md:text-sm px-6 pt-6">{{ post.category.displayName }}</p>
                 <div class="w-full font-bold text-xl text-gray-900 px-6">{{ post.title }}</div>
             </router-link>
         </div>
