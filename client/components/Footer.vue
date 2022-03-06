9164878f2292fc14a0dcfb7f9b4f308ec738a7ab
kristapsvitols <kristapsvitols91@gmail.com>
2022-03-06 16:29:09 +0200

    aria-label v2

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -16,7 +16,7 @@
                 <div class="flex w-full content-center justify-between md:w-1/3 md:justify-end">
                     <nav class="list-reset flex justify-center flex-1 md:flex-none items-center">
                         <a class="inline-block text-gray-200 no-underline hover:text-gray-200 py-2 px-3" href="#">Par mums</a>
-                        <a class="text-gray-200 inline-block no-underline hover:text-white hover:text-underline" href="https://www.facebook.com/Galamerkis.lv/" target="_blank" rel="noopener">
+                        <a class="text-gray-200 inline-block no-underline hover:text-white hover:text-underline" href="https://www.facebook.com/Galamerkis.lv/" target="_blank" rel="noopener" aria-label="Facebook">
                             <svg class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z"></path></svg>
                         </a>
                     </nav>
