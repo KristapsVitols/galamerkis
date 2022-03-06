52ed5f160e774ea7159b3d1be4f737a34c52999d
kristapsvitols <kristapsvitols91@gmail.com>
2022-03-06 11:23:05 +0200

    fix canonical v2

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -27,7 +27,6 @@ export default {
         },
         link: [
             {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-            {rel: 'canonical', href: 'https://www.galamerkis.lv/'},
         ],
     },
 
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,6 +7,13 @@ import MainPage from '../components/MainPage';
 export default {
     name: 'Index',
     components: {MainPage},
+    head() {
+        return {
+            link: [
+                {rel: 'canonical', href: 'https://www.galamerkis.lv/'},
+            ],
+        }
+    },
     async asyncData({store}) {
         try {
             await store.dispatch('getHomepageData');
